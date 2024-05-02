e0fc248d4429c439e537ff874175ef8655535342
DESKTOP-7HFNP5O\Dell <aditya098patel@gmail.com>
2024-05-02 11:47:41 +0530

    Initial commit

--- src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
+++ src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
@@ -1,18 +1,20 @@
 package org.apereo.cas.config;
 
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.apereo.cas.acct.AccountRegistrationPropertyLoader;
+import org.apereo.cas.acct.DefaultAccountRegistrationPropertyLoader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 //import org.apereo.cas.configuration.CasConfigurationProperties;
 
 @AutoConfiguration
 //@EnableConfigurationProperties(CasConfigurationProperties.class)
 public class CasOverlayOverrideConfiguration {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
     /*
     @Bean
@@ -20,4 +22,41 @@ public class CasOverlayOverrideConfiguration {
         ...
     }
      */
+
+	@Bean
+	public AccountRegistrationPropertyLoader accountMgmtRegistrationPropertyLoader() {
+	    return new DefaultAccountRegistrationPropertyLoader(null);
+	}
+	
+//	@Bean
+//	public AccountRegistrationProvisionerConfigurer customProvisioningConfigurer() {
+//	    return () -> {
+//	        return (request) ->{
+//	        	try {
+//	        		StringBuilder query = new StringBuilder();
+//		        	query.append(" INSERT INTO auth_user (first_name, last_name, email, password)");
+//		        	query.append(" VALUES (?, ?, ?, ?)");
+//		        	 
+//		        	jdbcTemplate.execute(query.toString(), new PreparedStatementCallback<Boolean>(){  
+//		        	    @Override  
+//		        	    public Boolean doInPreparedStatement(PreparedStatement ps)  
+//		        	            throws SQLException, DataAccessException {  
+//		        	              
+//			        	        ps.setString(1,request.getFirstName());  
+//			        	        ps.setString(2,request.getLastName());  
+//			        	        ps.setString(3,request.getEmail());  
+//			        	        ps.setString(3,request.getPassword());
+//			        	        
+//			        	        return ps.execute();  
+//		        	              
+//		        	    	}  
+//		        	    });
+//		        	return AccountRegistrationResponse.success();
+//	        	} catch(Exception e) {	
+//	        		return AccountRegistrationResponse.failure();
+//	        	}
+//	        	
+//	        };
+//	    };
+//	}
 }
