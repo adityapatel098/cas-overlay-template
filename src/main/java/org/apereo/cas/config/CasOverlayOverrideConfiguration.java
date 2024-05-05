eb237b7d203c45b6df0b3704cc65251b9cbc2131
DESKTOP-RATON1K\HP <a>
2024-05-05 12:29:50 +0530

    Account registration

--- src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
+++ src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
@@ -1,11 +1,18 @@
 package org.apereo.cas.config;
 
 import org.apereo.cas.acct.AccountRegistrationPropertyLoader;
+import org.apereo.cas.acct.AccountRegistrationResponse;
 import org.apereo.cas.acct.DefaultAccountRegistrationPropertyLoader;
+import org.apereo.cas.acct.provision.AccountRegistrationProvisionerConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 //import org.apereo.cas.configuration.CasConfigurationProperties;
 
@@ -28,35 +35,35 @@ public class CasOverlayOverrideConfiguration {
 	    return new DefaultAccountRegistrationPropertyLoader(null);
 	}
 	
-//	@Bean
-//	public AccountRegistrationProvisionerConfigurer customProvisioningConfigurer() {
-//	    return () -> {
-//	        return (request) ->{
-//	        	try {
-//	        		StringBuilder query = new StringBuilder();
-//		        	query.append(" INSERT INTO auth_user (first_name, last_name, email, password)");
-//		        	query.append(" VALUES (?, ?, ?, ?)");
-//		        	 
-//		        	jdbcTemplate.execute(query.toString(), new PreparedStatementCallback<Boolean>(){  
-//		        	    @Override  
-//		        	    public Boolean doInPreparedStatement(PreparedStatement ps)  
-//		        	            throws SQLException, DataAccessException {  
-//		        	              
-//			        	        ps.setString(1,request.getFirstName());  
-//			        	        ps.setString(2,request.getLastName());  
-//			        	        ps.setString(3,request.getEmail());  
-//			        	        ps.setString(3,request.getPassword());
-//			        	        
-//			        	        return ps.execute();  
-//		        	              
-//		        	    	}  
-//		        	    });
-//		        	return AccountRegistrationResponse.success();
-//	        	} catch(Exception e) {	
-//	        		return AccountRegistrationResponse.failure();
-//	        	}
-//	        	
-//	        };
-//	    };
-//	}
+	@Bean
+	public AccountRegistrationProvisionerConfigurer customProvisioningConfigurer() {
+	    return () -> {
+	        return (request) ->{
+	        	try {
+	        		StringBuilder query = new StringBuilder();
+	        	    query.append(" INSERT INTO auth_user (first_name, last_name, email, password)");
+		        	query.append(" VALUES (?, ?, ?, ?)");
+
+		        	jdbcTemplate.execute(query.toString(), new PreparedStatementCallback<Boolean>(){
+		        	    @Override
+		        	    public Boolean doInPreparedStatement(PreparedStatement ps)
+		        	            throws SQLException, DataAccessException {
+
+			        	        ps.setString(1,request.getFirstName());
+			        	        ps.setString(2,request.getLastName());
+			        	        ps.setString(3,request.getEmail());
+			        	        ps.setString(3,request.getPassword());
+
+			        	        return ps.execute();
+
+		        	    	}
+		        	    });
+		        	return AccountRegistrationResponse.success();
+	        	} catch(Exception e) {
+	        		return AccountRegistrationResponse.failure();
+	        	}
+
+	        };
+	    };
+	}
 }
